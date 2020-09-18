69c4bbd849af3d585530265161f60888959f2bea
mk_jojo <fdl.mail1@gmail.com>
2020-09-18 16:41:39 +0300

    headers template

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -5,7 +5,7 @@ const articlesRouter = new Router();
 
 articlesRouter.get(`/:id`, (req, res) => res.render(`post`));
 articlesRouter.get(`/add`, (req, res) => res.render(`new-post`));
-articlesRouter.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
+articlesRouter.get(`/edit/:id`, (req, res) => res.render(`new-post`));
 articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
 
 module.exports = articlesRouter;
